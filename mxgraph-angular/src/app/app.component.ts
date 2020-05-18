e1f9e7d3cdd328095819b6827bc727dcba7e85fd
vuonghuuthien <vuonghuuthientk@gmail.com>
2020-05-18 15:23:34 +0700

    Add, below code into app.component.ts file and app.component.html file

--- mxgraph-angular/src/app/app.component.ts
+++ mxgraph-angular/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
+declare var mxGraph: any;
+declare var mxHierarchicalLayout: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mxgraph-angular';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('graphContainer') graphContainer: ElementRef;
+  ngAfterViewInit() {
+    const graph = new mxGraph(this.graphContainer.nativeElement);
+    try {
+      const parent = graph.getDefaultParent();
+      graph.getModel().beginUpdate();
+      const vertex1 = graph.insertVertex(parent, '1', 'Vertex 1', 0, 0, 200, 80);
+      const vertex2 = graph.insertVertex(parent, '2', 'Vertex 2', 0, 0, 200, 80);
+      graph.insertEdge(parent, '', '', vertex1, vertex2);
+    } finally {
+      graph.getModel().endUpdate();
+      new mxHierarchicalLayout(graph).execute(graph.getDefaultParent());
+    }
+  }
 }
